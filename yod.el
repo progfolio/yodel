caf8647ecd550b01e2dcc5348521f8febc75e0a2
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-08-30 14:53:53 -0400

    Add plist* errors

--- yod.el
+++ yod.el
@@ -25,21 +25,28 @@
 ;;; Code:
 (require 'cl-lib)
 
-;; A vlist (variadic plist) is a special type of plist.
+;; A variadic plist is a strict subset of a plist.
 ;; Its keys must be keywords, its values may not be keywords.
+;; Empty keys are ignored.
+;; If a key is declared multiple times, it's last declaration is returned.
 ;; If a keyword ends with "*" all values until the next keyword
 ;; are associated with it in a list.
-(defun yod-vlist-to-plist (vlist)
-  "Convert VLIST to plist."
-  (let (plist variadic keyword)
-    (dolist (el vlist plist)
+(defun yod-plist*-to-plist (plist*)
+  "Convert PLIST* to plist."
+  (let (plist variadic keyword last)
+    (unless (keywordp (car plist*))
+      (signal 'wrong-type-argument `(keywordp ,(car plist*))))
+    (dolist (el plist* plist)
       (if (keywordp el)
           (setq variadic (string-suffix-p "*" (symbol-name el))
                 keyword el)
         (setq plist (plist-put plist keyword
                                (if variadic
                                    (append (plist-get plist keyword) (list el))
-                                 el)))))))
+                                 (unless (keywordp last)
+                                   (error "Non-variadic key \"%S\" associated with more than one value" keyword))
+                                 el))))
+      (setq last el))))
 
 (defun yod--position-point (indicator)
   "Replace point INDICATOR with actual point."
@@ -49,7 +56,7 @@
 
 (defmacro yod-file (&rest args)
   "ARGS."
-  (setq args (yod-vlist-to-plist args))
+  (setq args (yod-plist*-to-plist args))
   `(with-temp-buffer
      (insert ,(plist-get args :contents))
      (yod--position-point ,(or (plist-get args :point) "|"))
