1499457fdf865c7d4009e7fbe35c33e2f76948d3
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-09-07 16:15:11 -0400

    Add yodel macro

--- yod.el
+++ yod.el
@@ -127,5 +127,112 @@ Otherwise throw an error if PATH exists."
                  (delete-file ,file)))))
        ,return)))
 
+
+;;;###autoload
+(defmacro yodel (&rest args)
+  "Test elisp in a clean environment.
+ARGS may be any of the following keywords and their respective values:
+  - :pre* (Form)...
+      Forms evaluated before launching Emacs.
+
+  - :post* (Form)...
+      Forms evaluated in the testing environment after boostrapping.
+
+  - :interactive Boolean
+      If nil, the subprocess will immediately exit after the test.
+      Output will be printed to `yodel--process-buffer'
+      Otherwise, the subprocess will be interactive.
+
+  - :save Boolean
+      If non-nil, the :user-dir is not deleted after exiting.
+      Otherwise, it is immediately removed after the test is run.
+
+  - :executable String
+      Indicate the Emacs executable to launch.
+      Defaults to the path of the current Emacs executable.
+
+  - :raw Boolean
+      If non-nil, the raw process output is sent to
+      `yodel--process-buffer'. Otherwise, it is
+      passed to the :formatter function.
+
+  - :user-dir String
+      If non-nil, the test is run with `user-emacs-directory' set to STRING.
+      Otherwise, a temporary directory is created and used.
+      Unless absolute, paths are expanded relative to the variable
+      `temporary-file-directory'.
+
+ARGS are accessible within the :pre*/:post* phases via the
+locally bound plist, yodel-args."
+  (declare (indent defun))
+  (unless lexical-binding
+    (user-error "Lexical binding required for yodel"))
+  (let* ((preserve-files    (make-symbol "preserve-files"))
+         (temp-emacs-dir    (make-symbol "temp-emacs-dir"))
+         (interactive       (make-symbol "interactive"))
+         (emacs-args-symbol (make-symbol "emacs-args"))
+         (emacs-executable  (make-symbol "emacs-executable"))
+         (raw               (make-symbol "raw"))
+         (formatter         (make-symbol "formatter"))
+         (test              (make-symbol "test"))
+         (args              (yodel-plist*-to-plist args))
+         ;; Construct bug-report form
+         (temp-dir (if-let ((dir (plist-get args :user-dir)))
+                       (expand-file-name dir temporary-file-directory)
+                     (make-temp-file "yodel-" 'directory)))
+         (executable (or (plist-get args :executable)
+                         (concat invocation-directory invocation-name)))
+         ;; Construct metaprogram to be evaled by subprocess
+         (program (let ((print-level nil)
+                        (print-length nil))
+                    (pp-to-string
+                     ;; The top-level `let' is an intentional local
+                     ;; variable binding. We want users of
+                     ;; `yodel' to have access to their
+                     ;; args within :pre*/:post* programs. Since
+                     ;; we are binding with the package namespace, this
+                     ;; should not overwrite other user bindings.
+                     (append
+                      '(with-demoted-errors "Error: %S")
+                      `(,(append `(let ((yodel-args ',args)))
+                                 `((setq user-emacs-directory ,temp-dir
+                                         package-user-dir (expand-file-name "elpa" ,temp-dir)
+                                         default-directory ,temp-dir))
+                                 ;;@TODO: this needs to be earlier
+                                 ;; Do we need to create the user dir prior to running this?:
+                                 ;;(plist-get keywords :pre*)
+                                 `(,(append
+                                     '(unwind-protect)
+                                     `((progn ,@(plist-get args :post*))))))))))))
+    `(let* ((,preserve-files    ,(plist-get args :save))
+            (,interactive       ,(plist-get args :interactive))
+            (,emacs-executable  ,executable)
+            (,emacs-args-symbol (append (unless ,interactive '("--batch"))
+                                        ',yodel--default-args))
+            (,raw               ,(plist-get args :raw))
+            (,formatter         #',(or (plist-get args :formatter) 'yodel--format))
+            (,test              ,program)
+            (,temp-emacs-dir    ,temp-dir))
+       ;; Reset process buffer.
+       (with-current-buffer (get-buffer-create yodel--process-buffer)
+         (fundamental-mode)
+         (erase-buffer))
+       (make-process
+        :name yodel--process-buffer
+        :buffer yodel--process-buffer
+        :command `(,,emacs-executable ,@,emacs-args-symbol ,,test)
+        ;;@TODO: expose :filter to yodel
+        :sentinel (lambda (_process _event)
+                    (unless ,interactive
+                      (unless ,raw (funcall ,formatter ,args))
+                      (run-with-idle-timer
+                       1 nil (lambda ()
+                               (switch-to-buffer-other-window
+                                yodel--process-buffer))))
+                    (unless ,preserve-files
+                      (when (file-exists-p ,temp-emacs-dir)
+                        (delete-directory ,temp-emacs-dir 'recursive)))))
+       (message "Running yodel in directory: %s" ,temp-emacs-dir))))
+
 (provide 'yodel)
 ;;; yod.el ends here
