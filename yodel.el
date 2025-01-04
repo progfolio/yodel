812c2fd51c9efcc6cf7e326e304283a5e0d4c5af
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2025-01-04 07:53:17 -0500

    update copyright year

--- yodel.el
+++ yodel.el
@@ -1,6 +1,6 @@
 ;;; yodel.el --- Communicable Elisp                    -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021-2024 Nicholas Vollmer
+;; Copyright (C) 2021-2025 Nicholas Vollmer
 
 ;; Author:  Nicholas Vollmer
 ;; URL: https://github.com/progfolio/yodel
